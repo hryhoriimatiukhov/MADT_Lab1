c78a3984e3b1f2a98eba54e18961abb26832066b
Hryhorii Matiukhov <hryhorii.matiukhov@knf.stud.vu.lt>
2026-09-24 01:36:43 +0300

    Add third button to change background color

--- app/src/main/java/com/example/madt_lab1/MainActivity.kt
+++ app/src/main/java/com/example/madt_lab1/MainActivity.kt
@@ -5,24 +5,32 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
         val myTextView = findViewById<TextView>(R.id.textView)
         val myButton = findViewById<Button>(R.id.button)
         val btnColor = findViewById<Button>(R.id.button2)
+        val btnBgColor = findViewById<Button>(R.id.button3)
 
         // The first button changes the text
         myButton.setOnClickListener {
-            myTextView.text = "Bye World!"
+            myTextView.text = "Bye world!"
         }
 
-        // The second button changes the text colour to red
+        // The second button changes the text color
         btnColor.setOnClickListener {
             myTextView.setTextColor(Color.RED)
+        }
+
+        // The third button changes the background color of the screen
+        btnBgColor.setOnClickListener {
+            mainLayout.setBackgroundColor(Color.YELLOW)
         }
     }
 }
