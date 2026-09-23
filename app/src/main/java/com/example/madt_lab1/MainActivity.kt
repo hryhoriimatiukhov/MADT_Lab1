2996d61dcf5edb4254444267e1c60afde3f08612
Hryhorii Matiukhov <hryhorii.matiukhov@knf.stud.vu.lt>
2026-09-24 01:23:26 +0300

    Added button click handler to change text

--- app/src/main/java/com/example/madt_lab1/MainActivity.kt
+++ app/src/main/java/com/example/madt_lab1/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.madt_lab1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Find UI elements by their IDs
+        val myTextView = findViewById<TextView>(R.id.textView)
+        val myButton = findViewById<Button>(R.id.button)
+
+        // Handle button click event
+        myButton.setOnClickListener {
+            myTextView.text = "Bye World!"
         }
     }
 }
